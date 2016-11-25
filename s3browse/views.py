e17e636ffd0d565ea09bd8bde647a0393b44c738
Dave McLain <dmclain@gmail.com>
2016-11-24 22:24:16 -0600

    Initial Commit

---
+++ s3browse/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+import boto3
+
+def home(request):
+    s3 = boto3.resource('s3')
+    keys = []
+    for bucket in s3.buckets.all():
+        for key in bucket.objects.all():
+            keys.append(key.key)
+
+    return HttpResponse("<html><body>%s</body></html>" % "<br/>".join(keys))
